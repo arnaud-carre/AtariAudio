c0f31c84e2615b987a7df5f224cecd13baf52bd3
Arnaud Carré <arnaud.carre@gmail.com>
2026-09-09 12:08:46 +0200

    added TrapN callback

--- src/AtariMachine.cpp
+++ src/AtariMachine.cpp
@@ -70,7 +70,14 @@ void M68k_Reset_Callback(void* user)
 
 int M68k_Illegal_Callback(void* user, int opcode)
 {
-	assert(false);
+	//	assert(false);
+	return 1;
+}
+
+int M68k_TrapN_Callback(void* user, int n)
+{
+	AtariMachine* mch = (AtariMachine*)user;
+	mch->TrapInstructionCallback(n);
 	return 1;
 }
 
