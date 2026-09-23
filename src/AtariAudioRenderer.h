fe4fabbe931ab32aebf08008cb8e8d32f67caa61
Arnaud Carré <arnaud.carre@gmail.com>
2026-09-23 18:11:36 +0200

    first pass adding comments

--- src/AtariAudioRenderer.h
+++ src/AtariAudioRenderer.h
@@ -23,44 +23,44 @@ public:
 
 	struct SongInfo
 	{
-		int subsongCount;
-		int defaultSubsong;
-		int playerTickRate;
-		uint32_t 	hostReplayRate;
-		uint32_t ym2149Clock;
-		eFileType fileType;
-		const char* musicName;
-		const char* musicAuthor;
-		const char* ripper;
-		const char* converter;
-		const char* year;
-		const char* fileFormat;
-		const void* rawBinaryData;
-		uint32_t rawBinaryDataSize;
+		int subsongCount;					// amount of sub-song (.sndh could have several, .ym has 1)
+		int defaultSubsong;					// main subsong in case of multiple sub-songs
+		int playerTickRate;					// original music driver tick rate (typically 50Hz)
+		uint32_t 	hostReplayRate;			// emulation output rate (typically 48kHz)
+		uint32_t ym2149Clock;				// ym2149 audio chip clock (typically 2MHz for Atari ST)
+		eFileType fileType;					// file type (.sndh or .ym)
+		const char* musicName;				// music name or empty string ""
+		const char* musicAuthor;			// music author or empty string ""
+		const char* ripper;					// music ripper or empty string ""
+		const char* converter;				// music converter or empty string ""
+		const char* year;					// music year or empty string ""
+		const char* fileFormat;				// detail file format string ("SNDH", "YM 5", etc.)
+		const void* rawBinaryData;			// raw un-packed music file data
+		uint32_t rawBinaryDataSize;			// raw un-packed music file data size (in bytes)
 	};
 
-	// Create a AtariAudioRenderer instance from a .SNDH or .YM file data located in memory
-	// hostReplayRate is the rate you want to render audio stream ( ie 48000 for 48Khz )
+	// Create a AtariAudioRenderer instance from a .sndh or .ym file data located in memory
+	// hostReplayRate is the rate you want to render audio stream ( ie 48000 for 48kHz )
 	// defaultYm2149Clock is only used for some song file format that doesn't include ym clock (ym2 or ym3)
-	// The input SNDH data could be ICE! packed and .YM could be LHA packed
-	// After create you can free fileMemoryData if needed (AtariAudioRenderer keep an internal copy of the required data)
+	// The input .sndh file could be ICE! packed and .ym could be LZH packed
+	// After create you can free fileMemoryData if needed (AtariAudioRenderer keeps an internal copy of the required data)
 	static AtariAudioRenderer* Create(const void* fileMemoryData, uint32_t fileMemorySize, uint32_t hostReplayRate, uint32_t defaultYm2149Clock = kDefaultAtariYmClock);
 	static void Destroy(AtariAudioRenderer* ar);
 	
 	// Get information about the SNDH (like song name, author, amount of subsong, etc.)
 	const 	AtariAudioRenderer::SongInfo&	GetSongInfo() const { return m_songInfo; };
 
-	// Get a subsong duration in samples. 0 means there is no information about duration for this subsong
+	// Get a subsong duration in samples. 0 means there is no information in the file about duration for this subsong
 	virtual uint32_t GetSubsongDurationSample(int subsongId) const = 0;
 
 	// Initialize music driver to play a sub-song. By convention, subsongId starts at 1 (not 0)
-	// You must call InitSubSong before any call to AudioRender
+	// You must call InitSubSong before any call to AudioRender, even for a single sub song file (like .ym)
 	virtual bool	InitSubSong(int subSongId) = 0;
 
 	// Main audio rendering function.
 	// Compute the next "count" samples into "buffer" (mono, signed, 16bits samples)
-	// by default the song will loop. If you want to stop at the perfect end, you can
-	// use GetSubsongDurationSample() upfront to get exact amount of samples.
+	// By default the song will loop. If you want to stop at the perfect song duration, you can use GetSubsongDurationSample() upfront to get exact amount of samples to generate
+	// Of course rendering can be broken into smaller blocks by invoking AudioRender iteratively with reduced buffer sizes
 	virtual void AudioRender(int16_t* buffer, uint32_t count) = 0;
 
 	// Fast forward into the music. Doesn't output data, but perform full emulation
@@ -90,11 +90,11 @@ public:
 protected:
 	static	const	int		kSubsongCountMax = 128;
 
-	// Private constructors prevent direct instantiation
+	// preventing direct instantiation
 	virtual ~AtariAudioRenderer();
 	AtariAudioRenderer();
-    AtariAudioRenderer(const AtariAudioRenderer&) = delete;            // Prevent copy construction
-    AtariAudioRenderer& operator=(const AtariAudioRenderer&) = delete; // Prevent copy assignment
+    AtariAudioRenderer(const AtariAudioRenderer&) = delete;
+    AtariAudioRenderer& operator=(const AtariAudioRenderer&) = delete;
 
 	static eFileType QuickFileTypeCheck(const void* rawMemory, uint32_t rawSize);
 	uint16_t ReadBE16(const char* r);
